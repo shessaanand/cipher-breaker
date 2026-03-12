29d9029aa08de75f74fa697bda4d9cf07c00dd6a
Shessaanand Siva Sivamurugan <shessaasiva@gmail.com>
2026-03-12 17:22:25 +0530

    updated frequency.py

--- utils/frequency.py
+++ utils/frequency.py
@@ -1,3 +1,10 @@
+"""
+Date: 11/03/2026.
+Work Done today: 
+     1. Frequency Analysis of Caesar Ciphers
+"""
+
+
 ENGLISH_FREQ={'A':0.08167,'B':0.01492,'C':0.02782,'D':0.04253,'E':0.12702,'F':0.02228,'G':0.02015,'H':0.06094,'I':0.06966,'J':0.00153,'K':0.00772,'L':0.04025,'M':0.02406,'N':0.06749,'O':0.07507,'P':0.01929,'Q':0.00095,'R':0.05987,'S':0.06327,'T':0.09056,'U':0.02758,'V':0.00978,'W':0.02360,'X':0.00150,'Y':0.01974,'Z':0.00074}
 LETTERS_BY_FREQUENCY=sorted(ENGLISH_FREQ,key=ENGLISH_FREQ.get,reverse=True)
 
@@ -106,3 +113,4 @@ if __name__=="__main__":
 	print("\n"+"="*60)
 	print("EXPECTED: English scores LOWEST chi-squared, HIGHEST freq sum")
 	print("="*60)
+
